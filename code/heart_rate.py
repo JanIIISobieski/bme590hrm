e924cd2b9f680a7daf85b1d8a73b1962426c92e7
Gabriel Antoniak <gjantoniak@gmail.com>
2018-02-19 15:16:32 -0500

    Sets up skeleton for programming and testing

---
+++ code/heart_rate.py
@@ -0,0 +1,30 @@
+import pandas
+from numpy import amin, amax
+import logging
+import os
+
+
+class ECG:
+    def __init__():
+        pass
+
+    def import_csv():
+        pass
+
+    def find_mean_hr_bpm(self, time_dur=1):
+        pass
+
+    def find_volt_extrema(self):
+        pass
+
+    def find_duration(self):
+        pass
+
+    def find_num_beats(self):
+        pass
+
+    def find_beats(self):
+        pass
+
+    def export_json():
+        pass
---
+++ code/test_heart_rate.py
@@ -0,0 +1,8 @@
+def test_import():
+    pass
+
+def test_attributes():
+    pass
+
+def test_export():
+    pass
