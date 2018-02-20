8630f5b32dc9bea90db6625c1c29465b54885abe
Gabriel Antoniak <gjantoniak@gmail.com>
2018-02-19 23:38:57 -0500

    Completes CSV import of data (Closes #1)

--- code/heart_rate.py
+++ code/heart_rate.py
@@ -1,15 +1,25 @@
-import pandas
 from numpy import amin, amax
+import pandas
 import logging
+import matplotlib.pyplot as plt
 import os
 
 
 class ECG:
-    def __init__():
+    def __init__(self, filename='test_data1.csv'):
+        self.filename = filename
+        self.import_csv()
         pass
 
-    def import_csv():
-        pass
+    def import_csv(self):
+        full_file = os.path.join(os.path.dirname(__file__), '../test_data/',
+                                 self.filename)
+        imported_file = pandas.read_csv(full_file,
+                                        header=None,
+                                        names=['time', 'voltage'],
+                                        skipinitialspace=True)
+        self.time = imported_file.time.values
+        self.voltage = imported_file.voltage.values
 
     def find_mean_hr_bpm(self, time_dur=1):
         pass
