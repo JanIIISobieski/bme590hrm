bcc7a3ec028159ca49713bb5d274ec8be511926f
Gabriel Antoniak <gjantoniak@gmail.com>
2018-02-20 01:04:12 -0500

    Basic functionality of peak finding

--- code/heart_rate.py
+++ code/heart_rate.py
@@ -1,10 +1,3 @@
-from numpy import amin, amax
-import pandas
-import logging
-import matplotlib.pyplot as plt
-import os
-
-
 class ECG:
     def __init__(self, filename='test_data1.csv'):
         self.filename = filename
@@ -14,6 +7,9 @@ class ECG:
         pass
 
     def import_csv(self):
+        import pandas
+        import logging
+        import os
         full_file = os.path.join(os.path.dirname(__file__), '../test_data/',
                                  self.filename)
         imported_file = pandas.read_csv(full_file,
@@ -22,11 +18,13 @@ class ECG:
                                         skipinitialspace=True)
         self.time = imported_file.time.values
         self.voltage = imported_file.voltage.values
+        logging.info('Successfully imported CSV file')
 
     def find_mean_hr_bpm(self, time_dur=1):
         pass
 
     def find_volt_extrema(self):
+        from numpy import amin, amax
         self.voltage_extremes = (amin(self.voltage), amax(self.voltage))
 
     def find_duration(self):
@@ -36,6 +34,15 @@ class ECG:
         pass
 
     def find_beats(self):
+        import matplotlib.pyplot as plt
+        from scipy import signal
+        from numpy import arange
+        peakind = signal.find_peaks_cwt(self.voltage,
+                                        arange(1, 50),
+                                        min_snr=20)
+        plt.figure
+        plt.plot(self.time, self.voltage)
+        plt.plot(self.time[peakind], self.voltage[peakind], 'o')
         pass
 
     def export_json():
