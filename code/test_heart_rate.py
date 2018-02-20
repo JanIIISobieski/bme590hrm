5e39810aa15b91fb19619111b35e14e1f211c291
Gabriel Antoniak <gjantoniak@gmail.com>
2018-02-19 23:00:42 -0500

    Sets up testing of the CSV import method

--- code/test_heart_rate.py
+++ code/test_heart_rate.py
@@ -1,5 +1,17 @@
 def test_import():
-    pass
+    from heart_rate import ECG
+    first_set = ECG(filename="test_data1.csv")
+
+    assert first_set.time[0] == 0
+    assert first_set.time[-1] == 27.775
+    assert first_set.voltage[0] == -0.145
+    assert first_set.voltage[-1] == 0.72
+
+    second_set = ECG(filename="test_data27.csv")
+    assert second_set.time[0] == 0
+    assert second_set.time[-1] == 39.996
+    assert second_set.voltage[0] == -0.175
+    assert second_set.voltage[-1] == -1.7725
 
 
 def test_attributes():
